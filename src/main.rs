499385f3439f1f86104e641167dcd29edfa9eed0
aoki <aoki@aoki.dev>
2022-04-02 18:02:36 +0900

    read examples dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::{
+    fs::{self, read_dir},
+    path::PathBuf,
+};
+
+fn main() -> anyhow::Result<()> {
+    let example_dir = fs::canonicalize(PathBuf::from("./examples"))?;
+    println!("Examples directory: {}", &example_dir.to_string_lossy());
+    let x = read_dir(example_dir)?;
+    x.into_iter().for_each(|f| println!("{:?}", f));
+
     println!("Hello, example world!");
+    Ok(())
 }
