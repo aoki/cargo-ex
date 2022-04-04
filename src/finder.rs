d76284cf148747648ec6be93a7c9adea31087153
aoki <aoki@aoki.dev>
2022-04-04 21:29:56 +0900

    add fuzzy finder

--- src/finder.rs
+++ src/finder.rs
@@ -1,6 +1,1 @@
-fn finder() -> anyhow::Result<()> {
-    let skim_options = SkimOptionsBuilder::default()
-        .height(Some("50%"))
-        .build()
-        .map_err(|e| anyhow!(e))?;
-}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use anyhow::anyhow;
+use skim::{
+    prelude::{SkimItemReader, SkimOptionsBuilder},
+    Skim,
+};
 use std::{
-    ffi::{OsStr, OsString},
     fs::{self, read_dir},
+    io::Cursor,
     os::unix::prelude::CommandExt,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::Command,
 };
 
@@ -10,32 +15,45 @@ fn read_examples() -> anyhow::Result<()> {
     Ok(())
 }
 
+pub fn fuzzy_find(examples: Vec<String>) -> anyhow::Result<String> {
+    let skim_options = SkimOptionsBuilder::default()
+        .height(Some("50%"))
+        .build()
+        .map_err(|e| anyhow!(e))?;
+    let examples_string = examples.join("\n");
+    let item_reader = SkimItemReader::default().of_bufread(Cursor::new(examples_string));
+    let selected_items = Skim::run_with(&skim_options, Some(item_reader))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    let selected = selected_items
+        .iter()
+        .map(|i| i.output().to_string())
+        .collect::<Vec<String>>();
+
+    selected
+        .get(0)
+        .ok_or("no such an example file.")
+        .map_err(|e| anyhow!(e))
+        .map(|m| m.clone())
+}
+
 fn main() -> anyhow::Result<()> {
     let example_dir = fs::canonicalize(PathBuf::from("./examples"))?;
     println!("Examples directory: {}", &example_dir.to_string_lossy());
-    let x = read_dir(example_dir)?;
-    // x.into_iter().for_each(|f| println!("{:?}", f));
-    let z = x
+    let example_dir = read_dir(example_dir)?;
+    let examples = example_dir
         .into_iter()
-        .map(|f| {
-            let file = f.unwrap().file_name();
-            file
-        })
-        .collect::<Vec<OsString>>();
+        .map(|f| f.unwrap().file_name().to_string_lossy().to_string())
+        .collect::<Vec<String>>();
 
-    // let cmd = Command::new("cargo").arg("run --example").exec();
-    println!("{:?}", z[0]);
-    let cmd = Command::new("cargo")
+    let example = fuzzy_find(examples)?;
+
+    let _ = Command::new("cargo")
         .arg("run")
         .arg("--example")
-        .arg(
-            z[1].to_string_lossy()
-                .to_string()
-                .strip_suffix(".rs")
-                .unwrap(),
-        )
+        .arg(example.strip_suffix(".rs").unwrap())
         .exec();
 
-    println!("Hello, example world!");
     Ok(())
 }
